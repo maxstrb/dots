5c4ad265ca8e40243b87922e89e39b9373082e97
maxstrb <max.stribrny@gmail.com>
2025-09-26 17:33:32 +0200

    non important commit

--- home/modules/create-project/create-project.nix
+++ home/modules/create-project/create-project.nix
@@ -36,7 +36,7 @@
         mkdir $name
         cd $name
 
-        if (ls $"~/.nix-config/home/modules/create-project/project-blueprints/($type)" | length) > 0 {
+        if ($"~/.nix-config/home/modules/create-project/project-blueprints/($type)" | path expand | ls | length) > 0 {
           cp $"~/.nix-config/home/modules/create-project/project-blueprints/($type)/*" .
         }
 
