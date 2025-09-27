4225b32569d8f9d4c21b1b3465848a33c70739da
maxstrb <max.stribrny@gmail.com>
2025-09-27 14:16:57 +0200

    non important commit

--- home/modules/create-project/create-project.nix
+++ home/modules/create-project/create-project.nix
@@ -37,7 +37,7 @@
         cd $name
 
         if ($"~/.nix-config/home/modules/create-project/project-blueprints/($type)" | path expand | ls $in | length) > 0 {
-          glob $"~/.nix-config/home/modules/create-project/project-blueprints/($type)/*" | cp $in .
+          glob $"~/.nix-config/home/modules/create-project/project-blueprints/($type)/*" | each { |file| cp $file . }
         }
 
         if ($"./init.sh" | path type) == "file" {
