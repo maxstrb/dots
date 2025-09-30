24c035320ae3ae9509cc063f16fe512d5633425e
maxstrb <max.stribrny@gmail.com>
2025-09-30 12:00:12 +0200

    non important commit

--- home/modules/create-project/create-project.nix
+++ home/modules/create-project/create-project.nix
@@ -51,13 +51,13 @@
         }
 
         ssh $"admin@silvers.fun" $"sudo -S -u git git init --bare ~/($name).git"
-        print $"Repository ($name) created successfully!"
+        #print $"Repository ($name) created successfully!"
 
-        git init .
-        git add .
-        git commit -m "Project setup"
-        git $"remote add origin git@silvers.fun:($name).git"
-        git push origin main
+        #git init .
+        #git add .
+        #git commit -m "Project setup"
+        #git $"remote add origin git@silvers.fun:($name).git"
+        #git push origin main
 
         print "You are all done"
 
