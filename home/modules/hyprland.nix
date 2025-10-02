d650d269814a9ba5d2a05686f13e9f77ff92c512
maxstrb <max.stribrny@gmail.com>
2025-10-02 16:37:56 +0200

    non important commit

--- home/modules/hyprland.nix
+++ home/modules/hyprland.nix
@@ -103,6 +103,7 @@
         "$mainMod, code:23, swapsplit"
         "$mainMod, code:66, togglesplit"
         "Alt, F4, exec, hyprctl kill"
+        "$mainMod, exec, hyprlock"
 
         # switch focus
         "$mainMod, left,  movefocus, l"
@@ -193,6 +194,52 @@
 
       windowrule = [
         # "float,class:^(mpv)$"
+      ];
+    };
+  };
+
+  programs.hyprlock = {
+    enable = true;
+
+    settings = {
+      general = {
+        hide_cursor = true;
+        ignore_empty_input = true;
+      };
+
+      animations = {
+        enabled = true;
+        fade_in = {
+          duration = 300;
+          bezier = "easeOutQuint";
+        };
+        fade_out = {
+          duration = 300;
+          bezier = "easeOutQuint";
+        };
+      };
+
+      background = [
+        {
+          path = "screenshot";
+          blur_passes = 3;
+          blur_size = 8;
+        }
+      ];
+
+      input-field = [
+        {
+          size = "200, 50";
+          position = "0, -80";
+          monitor = "";
+          dots_center = true;
+          fade_on_empty = false;
+          font_color = "rgb(202, 211, 245)";
+          inner_color = "rgb(91, 96, 120)";
+          outer_color = "rgb(24, 25, 38)";
+          outline_thickness = 5;
+          shadow_passes = 2;
+        }
       ];
     };
   };
